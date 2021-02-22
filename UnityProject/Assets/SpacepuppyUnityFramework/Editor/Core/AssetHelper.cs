df2b4b76ca107942ca09020116d62cd28d60ff36
lordofduct <lordofduct@gmail.com>
2021-02-21 22:09:30 -0500

    Added dependencies from SP3.0 to get SPSerialization working in SP4.0

---
+++ UnityProject/Assets/SpacepuppyUnityFramework/Editor/Core/AssetHelper.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+using com.spacepuppy.Utils;
+
+namespace com.spacepuppyeditor
+{
+    public static class AssetHelper
+    {
+
+        public static string ProjectPath
+        {
+            get
+            {
+                return Path.GetDirectoryName(Application.dataPath);
+            }
+        }
+
+        public static string GetRelativeResourcePath(string spath)
+        {
+            if (string.IsNullOrEmpty(spath)) return string.Empty;
+
+            int i = spath.IndexOf("Resources") + 9;
+            if (i >= spath.Length) return string.Empty;
+
+            spath = spath.Substring(i);
+            spath = Path.Combine(Path.GetDirectoryName(spath), Path.GetFileNameWithoutExtension(spath)).Replace(@"\", "/");
+            return spath.EnsureNotStartWith("/");
+        }
+
+        public static void MoveFolder(string fromPath, string toPath)
+        {
+            fromPath = Path.Combine(AssetHelper.ProjectPath, fromPath);
+            toPath = Path.Combine(AssetHelper.ProjectPath, toPath);
+
+            if (Directory.Exists(fromPath) && !Directory.Exists(toPath))
+            {
+                Directory.Move(fromPath, toPath);
+                File.Move(fromPath + ".meta", toPath + ".meta");
+            }
+            else
+            {
+                throw new DirectoryNotFoundException();
+            }
+        }
+
+        public static string GetSelectedPath()
+        {
+            string path = "Assets";
+
+            foreach (var obj in Selection.GetFiltered(typeof(UnityEngine.Object), SelectionMode.Assets))
+            {
+                path = AssetDatabase.GetAssetPath(obj);
+                if (!string.IsNullOrEmpty(path) && System.IO.File.Exists(path))
+                {
+                    path = System.IO.Path.GetDirectoryName(path);
+                    break;
+                }
+            }
+
+            return path;
+        }
+
+    }
+}
---
+++ UnityProject/Assets/SpacepuppyUnityFramework/Framework/Core/Project/QueryableAssetSet.cs
@@ -0,0 +1,166 @@
+﻿#pragma warning disable 0649 // variable declared but not used.
+
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+using com.spacepuppy.Utils;
+
+namespace com.spacepuppy.Project
+{
+
+    [CreateAssetMenu(fileName = "AssetSet", menuName = "Spacepuppy/Asset Set")]
+    public class QueryableAssetSet : ScriptableObject, IAssetBundle
+    {
+
+        #region Fields
+
+        [SerializeField]
+        [ReorderableArray()]
+        private UnityEngine.Object[] _assets;
+
+        [System.NonSerialized]
+        private Dictionary<string, UnityEngine.Object> _table;
+        [System.NonSerialized]
+        private bool _clean;
+
+        #endregion
+
+        #region Methods
+
+        private void SetupTable()
+        {
+            if (_table == null)
+                _table = new Dictionary<string, Object>();
+            else
+                _table.Clear();
+
+            for (int i = 0; i < _assets.Length; i++)
+            {
+                _table[_assets[i].name] = _assets[i];
+            }
+            _clean = true;
+        }
+
+        public UnityEngine.Object GetAsset(string name)
+        {
+            if (!_clean) this.SetupTable();
+
+            UnityEngine.Object obj;
+            if (_table.TryGetValue(name, out obj))
+                return obj;
+            else
+                return null;
+        }
+
+        public IEnumerable<UnityEngine.Object> GetAllAssets()
+        {
+            if (!_clean) this.SetupTable();
+
+            return _table.Values;
+        }
+
+        public IEnumerable<T> GetAllAssets<T>() where T : class
+        {
+            if (!_clean) this.SetupTable();
+
+            var e = _table.Values.GetEnumerator();
+            while (e.MoveNext())
+            {
+                var obj = ObjUtil.GetAsFromSource<T>(e.Current);
+                if (!object.ReferenceEquals(obj, null))
+                {
+                    yield return obj;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Replaces the internal collection with a new set of assets.
+        /// </summary>
+        /// <param name="assets"></param>
+        public void ResetAssets(IEnumerable<UnityEngine.Object> assets)
+        {
+            _assets = assets.ToArray();
+            if (_table != null)
+            {
+                _table.Clear();
+                this.SetupTable();
+            }
+        }
+
+        #endregion
+
+        #region IAssetBundle Interface
+
+        public bool Contains(string name)
+        {
+            if (!_clean) this.SetupTable();
+
+            return _table.ContainsKey(name);
+        }
+
+        public bool Contains(UnityEngine.Object asset)
+        {
+            return System.Array.IndexOf(_assets, asset) >= 0;
+        }
+
+        public IEnumerable<string> GetAllAssetNames()
+        {
+            if (!_clean) this.SetupTable();
+
+            return _table.Keys;
+        }
+
+        UnityEngine.Object IAssetBundle.LoadAsset(string name)
+        {
+            return this.GetAsset(name);
+        }
+
+        UnityEngine.Object IAssetBundle.LoadAsset(string name, System.Type tp)
+        {
+            var obj = this.GetAsset(name);
+            if (object.ReferenceEquals(obj, null)) return null;
+
+            if (TypeUtil.IsType(obj.GetType(), tp)) return obj;
+            else return null;
+        }
+
+        T IAssetBundle.LoadAsset<T>(string name)
+        {
+            return this.GetAsset(name) as T;
+        }
+
+        public void UnloadAllAssets()
+        {
+            if (_table != null) _table.Clear();
+            for (int i = 0; i < _assets.Length; i++)
+            {
+                if (!(_assets[i] is GameObject))
+                {
+                    Resources.UnloadAsset(_assets[i]);
+                }
+            }
+            _clean = false;
+        }
+
+        public void UnloadAsset(UnityEngine.Object asset)
+        {
+            if (this.Contains(asset))
+            {
+                Resources.UnloadAsset(asset);
+            }
+        }
+
+
+
+        public void Dispose()
+        {
+            Resources.UnloadAsset(this);
+        }
+
+        #endregion
+
+    }
+
+}

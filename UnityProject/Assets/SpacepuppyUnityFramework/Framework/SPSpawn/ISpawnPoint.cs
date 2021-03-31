266da4ea80ea814c6e8216bfd027deed61d51051
lordofduct <lordofduct@gmail.com>
2021-03-30 23:36:53 -0400

    Added SPSpawn lib

---
+++ UnityProject/Assets/SpacepuppyUnityFramework/Framework/SPSpawn/ISpawnPoint.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+using com.spacepuppy.Events;
+
+namespace com.spacepuppy.Spawn
+{
+
+    public interface ISpawnPoint
+    {
+
+        BaseSPEvent OnSpawned { get; }
+
+        void Spawn();
+
+    }
+
+}

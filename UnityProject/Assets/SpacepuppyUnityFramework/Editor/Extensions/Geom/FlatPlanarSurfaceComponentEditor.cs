f92d8ba5a2baa38dd8ee17350b83497498d75f23
lordofduct <lordofduct@gmail.com>
2021-05-15 17:55:40 -0400

    Added FlatPlanarSurfaceComponent

---
+++ UnityProject/Assets/SpacepuppyUnityFramework/Editor/Extensions/Geom/FlatPlanarSurfaceComponentEditor.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+using System.Linq;
+
+using com.spacepuppy.Geom;
+
+namespace com.spacepuppyeditor.Geom
+{
+
+    [CustomEditor(typeof(FlatPlanarSurfaceComponent))]
+    public class FlatPlanarSurfaceComponentEditor : SPEditor
+    {
+
+
+        [DrawGizmo(GizmoType.Selected, drawnType = typeof(FlatPlanarSurfaceComponent))]
+        static void DrawGizmos(FlatPlanarSurfaceComponent surface, GizmoType gizmoType)
+        {
+            var c = Color.green;
+            c.a = 0.3f;
+            Gizmos.color = c;
+            Gizmos.matrix = Matrix4x4.TRS(surface.transform.position, surface.transform.rotation, Vector3.one);
+
+            Gizmos.DrawCube(Vector3.zero, new Vector3(10f, 10f, 0.05f));
+
+            Gizmos.matrix = Matrix4x4.identity;
+        }
+
+
+    }
+
+}
--- UnityProject/Assets/SpacepuppyUnityFramework/Framework/SPGeom/Geom/IPlanarSurface.cs
+++ UnityProject/Assets/SpacepuppyUnityFramework/Framework/SPGeom/Geom/IPlanarSurface.cs
@@ -11,12 +11,23 @@ namespace com.spacepuppy.Geom
     public interface IPlanarSurface
     {
 
+        /// <summary>
+        /// Returns the surface normal at the 2D surface location.
+        /// </summary>
+        /// <param name="location"></param>
+        /// <returns></returns>
         Vector3 GetSurfaceNormal(Vector2 location);
 
+        /// <summary>
+        /// Return the surface normal at the position on the curve closest to location.
+        /// </summary>
+        /// <param name="location"></param>
+        /// <returns></returns>
+        /// <remarks>When implementing the location isn't necessarily going to be on the surface so it should be treated as the point resolved from ClampToSurface.</remarks>
         Vector3 GetSurfaceNormal(Vector3 location);
 
         /// <summary>
-        /// Converts a 3d vector to closest 2d vector on 2D gameplay surface
+        /// Converts a 3d vector to closest 2d vector on 2D surface
         /// </summary>
         /// <param name="v"></param>
         /// <returns></returns>
@@ -49,6 +60,36 @@ namespace com.spacepuppy.Geom
         /// <param name="v"></param>
         /// <returns></returns>
         Vector3 ClampToSurface(Vector3 v);
+
+    }
+
+    public static class PlanarSurfaceExtensions
+    {
+
+        public static Vector3 MirrorPosition(this IPlanarSurface surface, Vector3 pos)
+        {
+            var cpos = surface.ClampToSurface(pos);
+            var pn = surface.GetSurfaceNormal(cpos);
+            var pd = Vector3.Dot(cpos, pn);
+
+            //var d = Vector3.Dot(pn, (pos - pn * pd));
+            var d = Vector3.Dot(pn, pos) - pd; //this way is just fewer calculations, both work
+            return pos - pn * d * 2f;
+        }
+
+        public static Vector3 MirrorDirection(this IPlanarSurface surface, Vector3 pos, Vector3 v)
+        {
+            var pn = surface.GetSurfaceNormal(pos);
+            var pd = Vector3.Dot(v, pn);
+            return v - pn * pd * 2f;
+        }
+
+        public static Quaternion GetMirrorLookRotation(this IPlanarSurface surface, Vector3 pos, Vector3 forw, Vector3 up)
+        {
+            forw = surface.MirrorDirection(pos, forw);
+            up = surface.MirrorDirection(pos, up);
+            return Quaternion.LookRotation(forw, up);
+        }
 
     }
 

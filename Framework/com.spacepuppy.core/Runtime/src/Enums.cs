be58558115a188087152da842aee8f399ab26af2
lordofduct <lordofduct@gmail.com>
2021-12-10 18:49:51 -0500

    Reorganized entire project into standard Unity Package layout on a per module basis.

---
+++ Framework/com.spacepuppy.core/Runtime/src/Enums.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+
+namespace com.spacepuppy
+{
+
+    public enum VariantType : sbyte
+    {
+        Object = -1,
+        Null = 0,
+        String = 1,
+        Boolean = 2,
+        Integer = 3,
+        Float = 4,
+        Double = 5,
+        Vector2 = 6,
+        Vector3 = 7,
+        Vector4 = 8,
+        Quaternion = 9,
+        Color = 10,
+        DateTime = 11,
+        GameObject = 12,
+        Component = 13,
+        LayerMask = 14,
+        Rect = 15,
+        Numeric = 16,
+        Ref = 17,
+    }
+
+    public enum QuitState
+    {
+        None,
+        BeforeQuit,
+        Quit
+    }
+
+    public enum EntityRelativity
+    {
+        Entity = 0,
+        Self = 1,
+        SelfAndChildren = 2
+    }
+
+    /// <summary>
+    /// Search parameter type
+    /// </summary>
+    public enum SearchBy
+    {
+        Nothing = 0,
+        Tag = 1,
+        Name = 2,
+        Type = 3
+    }
+
+    /// <summary>
+    /// Describe an axis in cartesian coordinates, Useful for components that need to serialize which axis to use in some fashion.
+    /// </summary>
+    public enum CartesianAxis
+    {
+        Zneg = -3,
+        Yneg = -2,
+        Xneg = -1,
+        X = 0,
+        Y = 1,
+        Z = 2
+    }
+
+    [System.Flags()]
+    public enum ComparisonOperator
+    {
+        NotEqual = 0,
+        LessThan = 1,
+        GreaterThan = 2,
+        NotEqualAlt = 3,
+        Equal = 4,
+        LessThanEqual = 5,
+        GreatThanEqual = 6,
+        Always = 7
+    }
+
+    /// <summary>
+    /// Enum used by InfoboxAttribute to define which message box type to display as.
+    /// </summary>
+    public enum InfoBoxMessageType
+    {
+        None = 0,
+        Info = 1,
+        Warning = 2,
+        Error = 3
+    }
+
+    public enum AudioInterruptMode
+    {
+        StopIfPlaying = 0,
+        DoNotPlayIfPlaying = 1,
+        PlayOverExisting = 2
+    }
+
+}

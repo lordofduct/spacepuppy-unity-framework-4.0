15ca42c1fea7c5de30bd9d57916975c405a9e655
lordofduct <lordofduct@gmail.com>
2023-05-10 17:22:16 -0400

    Made the infobox decorator retractable

--- Framework/com.spacepuppy.core/Editor/src/Core/InfoboxDecorator.cs
+++ Framework/com.spacepuppy.core/Editor/src/Core/InfoboxDecorator.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 
 using com.spacepuppy;
+using com.spacepuppyeditor.Internal;
 
 namespace com.spacepuppyeditor.Core
 {
@@ -14,6 +15,22 @@ namespace com.spacepuppyeditor.Core
 
         private const float MARGIN = 8f;
         private const float MARGIN_DBL = MARGIN * 2f;
+
+        private static bool _showInfo;
+        private static bool ShowInfo
+        {
+            get => _showInfo;
+            set
+            {
+                if (_showInfo == value) return;
+                _showInfo = value;
+                EditorPrefs.SetBool("SPEditor.ShowInfoboxDecorator", value);
+            }
+        }
+        static InfoboxDecorator()
+        {
+            _showInfo = EditorPrefs.GetBool("SPEditor.ShowInfoboxDecorator", false);
+        }
 
         public override float GetHeight()
         {
@@ -28,13 +45,32 @@ namespace com.spacepuppyeditor.Core
 
         public static float GetHeight(InfoboxAttribute attrib)
         {
-            GUIStyle style = GUI.skin.GetStyle("HelpBox");
-            return Mathf.Max(20f, style.CalcHeight(new GUIContent(attrib.Message), EditorGUIUtility.currentViewWidth - MARGIN_DBL)) + EditorGUIUtility.singleLineHeight;
+            if (ShowInfo)
+            {
+                GUIStyle style = GUI.skin.GetStyle("HelpBox");
+                return Mathf.Max(20f, style.CalcHeight(new GUIContent(attrib.Message), EditorGUIUtility.currentViewWidth - MARGIN_DBL)) + EditorGUIUtility.singleLineHeight;
+            }
+            else
+            {
+                return EditorGUIUtility.singleLineHeight;
+            }
         }
 
         public static void OnGUI(Rect position, InfoboxAttribute attrib)
         {
-            EditorGUI.HelpBox(new Rect(position.xMin + MARGIN, position.yMin, position.width - MARGIN_DBL, position.height), attrib.Message, (MessageType)attrib.MessageType);
+            if (ShowInfo)
+            {
+                EditorGUI.HelpBox(new Rect(position.xMin + MARGIN, position.yMin, position.width - MARGIN_DBL, position.height), attrib.Message, (MessageType)attrib.MessageType);
+            }
+            else
+            {
+                EditorGUI.HelpBox(new Rect(position.xMin + MARGIN, position.yMin, position.width - MARGIN_DBL, position.height), "click to expand...", (MessageType)attrib.MessageType);
+            }
+
+            if (ReorderableListHelper.IsClickingArea(position))
+            {
+                ShowInfo = !ShowInfo;
+            }
         }
 
     }

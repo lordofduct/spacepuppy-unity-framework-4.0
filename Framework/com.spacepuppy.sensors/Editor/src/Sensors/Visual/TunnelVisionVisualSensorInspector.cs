07194269ac23114a7d6d52755c353d2f79986597
lordofduct <lordofduct@gmail.com>
2023-06-17 14:04:39 -0400

    Added multiobject editing support to TunnelVisionVisualSensorInspector.

--- Framework/com.spacepuppy.sensors/Editor/src/Sensors/Visual/TunnelVisionVisualSensorInspector.cs
+++ Framework/com.spacepuppy.sensors/Editor/src/Sensors/Visual/TunnelVisionVisualSensorInspector.cs
@@ -11,6 +11,7 @@ namespace com.spacepuppyeditor.Sensors.Visual
 {
 
     [CustomEditor(typeof(TunnelVisionVisualSensor))]
+    [CanEditMultipleObjects]
     public class TunnelVisionVisualSensorInspector : SPEditor
     {
 

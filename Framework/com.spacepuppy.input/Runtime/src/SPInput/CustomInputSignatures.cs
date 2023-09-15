df0e75ecdbec7f0a9a297cb4b9c6d5ab5fb0deb6
lordofduct <lordofduct@gmail.com>
2023-09-15 16:10:50 -0400

    Added MultiClickButton to SPInput signatures.

--- Framework/com.spacepuppy.input/Runtime/src/SPInput/CustomInputSignatures.cs
+++ Framework/com.spacepuppy.input/Runtime/src/SPInput/CustomInputSignatures.cs
@@ -166,7 +166,7 @@ namespace com.spacepuppy.SPInput
                     _delay = Mathf.Lerp(_delay, _maxRepeat, _repeatLerp);
                 _repeatCount++;
             }
-            else if(_current == ButtonState.Released)
+            else if (_current == ButtonState.Released)
             {
                 _lastUp = Time.unscaledTimeAsDouble;
             }
@@ -303,6 +303,220 @@ namespace com.spacepuppy.SPInput
                     case ButtonState.Down:
                         {
                             _count++;
+                            _lastRealDown = Time.unscaledTimeAsDouble;
+
+                            if (_count == _taps)
+                            {
+                                _current = ButtonState.Down;
+                                _lastDown = Time.unscaledTimeAsDouble;
+                            }
+                        }
+                        break;
+                    default:
+                        _current = ButtonState.None;
+                        if (Time.unscaledTimeAsDouble - _lastRealDown > _delay)
+                        {
+                            _count = 0;
+                        }
+                        break;
+                }
+            }
+            else if (_current > ButtonState.None)
+            {
+                switch (_realState)
+                {
+                    case ButtonState.Released:
+                    case ButtonState.None:
+                        _current = ButtonState.Released;
+                        _lastUp = Time.unscaledTimeAsDouble;
+                        _count = 0;
+                        break;
+                    default:
+                        _current = ButtonState.Held;
+                        break;
+                }
+            }
+            else
+            {
+                _current = ButtonState.None;
+                _count = 0;
+            }
+        }
+
+        public override void FixedUpdate()
+        {
+            _currentFixed = InputUtil.GetNextFixedButtonStateFromCurrent(_currentFixed, _current);
+        }
+
+        public override void Reset()
+        {
+            _current = ButtonState.None;
+            _currentFixed = ButtonState.None;
+            _lastDown = double.NegativeInfinity;
+            _lastUp = double.NegativeInfinity;
+
+            _realState = ButtonState.None;
+            _lastRealDown = 0d;
+            _count = 0;
+        }
+
+        #endregion
+
+    }
+
+    /// <summary>
+    /// Similar to MultiTapButton, but it validates a cursor position to register a valid repeat press of the button. 
+    /// Used to simulate something like a 'double-click' on the same spot.
+    /// </summary>
+    public class MultiClickButton : BaseInputSignature, IButtonInputSignature
+    {
+
+        #region Fields
+
+        private ButtonDelegate _delegate;
+        private int _taps;
+        private float _delay;
+        private System.Func<Vector2> _cursorPosDel;
+        private float _cursorPosTolerance;
+
+        private ButtonState _current;
+        private ButtonState _currentFixed;
+        private double _lastDown = double.NegativeInfinity;
+        private double _lastUp = double.NegativeInfinity;
+        private Vector2 _lastPressPos;
+
+        private ButtonState _realState;
+        private int _count;
+        private double _lastRealDown;
+
+        #endregion
+
+        #region CONSTRUCTOR
+
+        public MultiClickButton(string id, ButtonDelegate del, int taps, float delay, System.Func<Vector2> cursorPosDel, float cursorPosTolerance) : base(id)
+        {
+            _delegate = del;
+            _taps = Mathf.Max(1, taps);
+            _delay = delay;
+            _cursorPosDel = cursorPosDel;
+            _cursorPosTolerance = cursorPosTolerance;
+
+            _realState = ButtonState.None;
+            _count = 0;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public ButtonDelegate Delegate
+        {
+            get { return _delegate; }
+            set { _delegate = value; }
+        }
+
+        public int Taps
+        {
+            get { return _taps; }
+            set { _taps = Mathf.Max(1, value); }
+        }
+
+        public float Delay
+        {
+            get { return _delay; }
+            set { _delay = value; }
+        }
+
+        public System.Func<Vector2> CursorPosDel
+        {
+            get => _cursorPosDel;
+            set => _cursorPosDel = value;
+        }
+
+        public float CursorPosTolerance
+        {
+            get => _cursorPosTolerance;
+            set => _cursorPosTolerance = value;
+        }
+
+        #endregion
+
+        #region IButtonInputSignature Interface
+
+        public ButtonState CurrentState
+        {
+            get
+            {
+                if (GameLoop.CurrentSequence == UpdateSequence.FixedUpdate)
+                {
+                    return _currentFixed;
+                }
+                else
+                {
+                    return _current;
+                }
+            }
+        }
+
+        public double LastDownTime => _lastDown;
+
+        public double LastReleaseTime => _lastUp;
+
+        public void Consume()
+        {
+            if (GameLoop.CurrentSequence == UpdateSequence.FixedUpdate)
+            {
+                _currentFixed = InputUtil.ConsumeButtonState(_currentFixed);
+            }
+            else
+            {
+                _current = InputUtil.ConsumeButtonState(_current);
+            }
+        }
+
+        public ButtonState GetCurrentState(bool getFixedState)
+        {
+            return (getFixedState) ? _currentFixed : _current;
+        }
+
+        #endregion
+
+        #region IInputSignature Interfacce
+
+        public override void Update()
+        {
+            _realState = InputUtil.GetNextButtonState(_realState, _delegate != null ? _delegate() : false);
+            if (_count < _taps)
+            {
+                switch (_realState)
+                {
+                    case ButtonState.Down:
+                        if (_count == 0)
+                        {
+                            _lastPressPos = _cursorPosDel?.Invoke() ?? default;
+                            _count = 1;
+                            _lastRealDown = Time.unscaledTimeAsDouble;
+
+                            if (_count == _taps)
+                            {
+                                _current = ButtonState.Down;
+                                _lastDown = Time.unscaledTimeAsDouble;
+                            }
+                        }
+                        else
+                        {
+                            var pos = _cursorPosDel?.Invoke() ?? default;
+                            float dist = (pos - _lastPressPos).sqrMagnitude;
+                            _lastPressPos = pos;
+                            if (dist > _cursorPosTolerance * _cursorPosTolerance)
+                            {
+                                _count = 1;
+                            }
+                            else
+                            {
+                                _count++;
+                            }
+
                             _lastRealDown = Time.unscaledTimeAsDouble;
 
                             if (_count == _taps)

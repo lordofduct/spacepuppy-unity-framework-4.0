ad213aad95a49f9a0d25df8213798a44682d2cfb
lordofduct <lordofduct@gmail.com>
2024-09-29 19:26:10 -0400

    Added NullableFloat

--- Framework/com.spacepuppy.core/Editor/src/Core/DiscreteFloatPropertyDrawer.cs
+++ Framework/com.spacepuppy.core/Editor/src/Core/DiscreteFloatPropertyDrawer.cs
@@ -1,9 +1,9 @@
 ﻿using UnityEngine;
 using UnityEditor;
+using System.Reflection;
 
 using com.spacepuppy;
 using com.spacepuppy.Utils;
-using System.Reflection;
 
 namespace com.spacepuppyeditor.Core
 {
@@ -99,4 +99,84 @@ namespace com.spacepuppyeditor.Core
         }
 
     }
+
+    [CustomPropertyDrawer(typeof(NullableFloat))]
+    public class NullableFloatPropertyDrawer : PropertyDrawer
+    {
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return EditorGUIUtility.singleLineHeight;
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            const float WIDTH_INFTOGGLE = 62f;
+
+            string shortlabel, longlabel;
+            GetLabels(out shortlabel, out longlabel);
+
+            position = SPEditorGUI.SafePrefixLabel(position, label);
+            var r0 = new Rect(position.xMin, position.yMin, WIDTH_INFTOGGLE, EditorGUIUtility.singleLineHeight);
+            var r1 = new Rect(r0.xMax, r0.yMin, Mathf.Max(0f, position.width - r0.width), EditorGUIUtility.singleLineHeight);
+
+            var prop_hasvalue = property.FindPropertyRelative("_hasValue");
+            var prop_value = property.FindPropertyRelative("_value");
+
+            bool hasvalue = prop_hasvalue.boolValue;
+            EditorGUI.BeginChangeCheck();
+            hasvalue = !EditorGUI.ToggleLeft(r0, shortlabel, !hasvalue);
+            if (EditorGUI.EndChangeCheck())
+            {
+                prop_hasvalue.boolValue = hasvalue;
+                prop_value.floatValue = hasvalue ? NormalizeBasedOnAttribs(0f) : 0f;
+            }
+
+            if (hasvalue)
+            {
+                EditorGUI.BeginChangeCheck();
+                float value = EditorGUI.FloatField(r1, prop_value.floatValue);
+                if (EditorGUI.EndChangeCheck())
+                {
+                    value = NormalizeBasedOnAttribs(value);
+                    prop_value.floatValue = value;
+                }
+            }
+            else
+            {
+                prop_value.floatValue = 0f;
+                EditorGUI.BeginChangeCheck();
+                string soutput = EditorGUI.DelayedTextField(r1, longlabel, GUI.skin.textField);
+                if (EditorGUI.EndChangeCheck())
+                {
+                    if (float.TryParse(soutput, out float output))
+                    {
+                        prop_hasvalue.boolValue = true;
+                        prop_value.floatValue = NormalizeBasedOnAttribs(output);
+                    }
+                }
+            }
+        }
+
+        void GetLabels(out string shortlabel, out string longlabel)
+        {
+            var attrib = this.fieldInfo?.GetCustomAttribute<NullableFloat.LabelAttribute>();
+            shortlabel = attrib?.ShortLabel ?? "null";
+            longlabel = attrib?.LongLabel ?? "NULL";
+        }
+
+        private float NormalizeBasedOnAttribs(float value)
+        {
+            var attribs = this.fieldInfo?.GetCustomAttributes(typeof(NullableFloat.ConfigAttribute), false) as NullableFloat.ConfigAttribute[];
+            if (attribs == null) return value;
+
+            foreach (var attrib in attribs)
+            {
+                value = attrib.Normalize(value);
+            }
+            return value;
+        }
+
+    }
+
 }

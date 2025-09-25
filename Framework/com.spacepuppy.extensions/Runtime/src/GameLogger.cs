9724cbefb2a6f31268bbecce6951b95594dcf0e9
DYLAN-MINIPC\lordo <lordofduct@gmail.com>
2025-09-25 16:48:17 -0400

    Added GameLogger.TryGetMonoContext.

--- Framework/com.spacepuppy.extensions/Runtime/src/GameLogger.cs
+++ Framework/com.spacepuppy.extensions/Runtime/src/GameLogger.cs
@@ -84,5 +84,51 @@ namespace com.spacepuppy
 #endif
         }
 
+
+
+        /// <summary>
+        /// Returns a context object for logging if and only if it's the first to call the method. If the object dies the next object 
+        /// of that type to call will take over as context. This is ueful for logging for only one instance of a class that there are 
+        /// multiple instances of.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <param name="forceContext"></param>
+        /// <returns></returns>
+        public static LogContext<T> TryGetMonoContext<T>(this T context, bool forceContext = false) where T : UnityEngine.Object
+        {
+#if GAMELOGGER_ENABLED
+            if (context != null && (forceContext || LogContext<T>.context == null || LogContext<T>.context == context))
+            {
+                LogContext<T>.context = context;
+                return LogContext<T>.instance;
+            }
+#endif
+            return null;
+        }
+        public class LogContext<T> where T : UnityEngine.Object
+        {
+
+            internal static T context;
+            internal static readonly LogContext<T> instance = new();
+
+            [Conditional("UNITY_EDITOR"), Conditional("DEBUG"), Conditional("UNITY_DEBUG"), Conditional("ENABLE_GAMELOGGER")]
+            public void Log(object message)
+            {
+#if GAMELOGGER_ENABLED
+                UnityEngine.Debug.Log(message);
+#endif
+            }
+
+            [Conditional("UNITY_EDITOR"), Conditional("DEBUG"), Conditional("UNITY_DEBUG"), Conditional("ENABLE_GAMELOGGER")]
+            public void Log(object message, UnityEngine.Object context)
+            {
+#if GAMELOGGER_ENABLED
+                UnityEngine.Debug.Log(message, context);
+#endif
+            }
+
+        }
+
     }
 }
